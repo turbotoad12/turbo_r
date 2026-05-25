fee7be017df24108c06b37da154c8f9f65fe7199
Zachary Jones <jones_zachary@outlook.com>
2026-05-25 18:59:49 -0500

    0.1.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+/*
+Prints a string with a newline;
+ */
+pub fn print_str(s: &str) {
+    println!("{}", s);
+}   
