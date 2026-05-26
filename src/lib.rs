4119b03a758640cf40649a65df74ab6a8a0f3147
TurboToad12 <jones_zachary@outlook.com>
2026-05-25 19:33:22 -0500

    0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,12 @@ Prints a string with a newline;
  */
 pub fn print_str(s: &str) {
     println!("{}", s);
-}   
+}
+
+pub mod git {
+    use git2::Repository;
+    pub fn git_clone(url: &str, dst: &str) -> Result<(), git2::Error> {
+        Repository::clone(url, dst)?;
+        Ok(())
+    }
+}
