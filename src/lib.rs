605e1663e92dae5df2f7c9f9c6add3c88efc723b
TurboToad12 <jones_zachary@outlook.com>
2026-05-25 20:25:37 -0500

    Remove this deprecated and unsused stuffs.

--- src/lib.rs
+++ src/lib.rs
@@ -8,15 +8,6 @@ pub fn print_str(s: &str) {
     println!("{}", s);
 }
 
-#[deprecated]
-pub mod git {
-    use git2::Repository;
-    pub fn git_clone(url: &str, dst: &str) -> Result<(), git2::Error> {
-        Repository::clone(url, dst)?;
-        Ok(())
-    }
-}
-
 pub mod fs {
     use std::fs;
     pub fn delete_dir_recursive(dir: &str) {
